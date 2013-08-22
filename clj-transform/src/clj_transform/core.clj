2db2e4a83fc3f6a2f6780d3f71df25e4cf2591c2
Hugo <hugo.firth@gmail.com>
2013-08-22 22:09:41 +1000

    formatting xml into a nice (ish) map

--- clj-transform/src/clj_transform/core.clj
+++ clj-transform/src/clj_transform/core.clj
@@ -1,6 +1,14 @@
-(ns clj-transform.core)
+(ns clj-transform.core
+  (:require [clojure.xml :as xml]
+            [clojure.zip :as zip])
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;copy-pasta from clojure docs
+(defn zip-str [s]
+  (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+
+(defn x-to-j [xml-data]
+  (let [parsed-data (zip-str xml-data)
+        non-nil-vals (filter #(not= % nil) parsed-data)]
+    (first non-nil-vals)
+ ))
--- clj-transform/test/clj_transform/core_test.clj
+++ clj-transform/test/clj_transform/core_test.clj
@@ -1,7 +1,23 @@
 (ns clj-transform.core-test
-  (:require [clojure.test :refer :all]
-            [clj-transform.core :refer :all]))
+  (:use midje.sweet)
+  (:require [clj-transform.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(facts "xml to map transform"
+  (fact "can transform a single element to map"
+        (x-to-j "<a/>") => { :tag :a :content nil :attrs nil })
+  (fact "can transform a single element with a value"
+        (x-to-j "<a>v</a>") => { :tag :a :content ["v"] :attrs nil})
+  (fact "can transform multiple elements"
+        (x-to-j "<g><a>v1</a><b>v2</b></g>") => {:tag :g 
+                                                 :attrs nil
+                                                 :content [{:tag :a 
+                                                            :content ["v1"] 
+                                                            :attrs nil}
+                                                           {:tag :b 
+                                                            :content ["v2"] 
+                                                            :attrs nil}]})
+        
+
+
+        )
