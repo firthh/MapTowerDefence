39ac3f636a8693a0761be5992a5875f801bbb4e9
Hugo <hugo.firth@gmail.com>
2013-08-23 09:30:20 +1000

    added basis for getting data from OSM

--- clj-transform/project.clj
+++ clj-transform/project.clj
@@ -5,13 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [org.clojure/data.json "0.2.2"]]
+                 [org.clojure/data.json "0.2.2"]
+                 [http.async.client "0.5.2"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler clj-transform.web/app
          :init    clj-transform.web/init
          :destroy clj-transform.web/destroy}
   :profiles {:dev {
-                   :plugins [[lein-midje "3.0.0"]]
+                   :plugins [[lein-midje "3.0.0"]
+                             [lein-ancient "0.4.0"]]
                    :dependencies [[midje "1.6-alpha2"]
                                   [ring-mock "0.1.5"]]}
              })
---
+++ clj-transform/src/clj_transform/http_wrapper.clj
@@ -0,0 +1,29 @@
+(ns clj-transform.http-wrapper
+  (:require [clojure.data.json :refer (read-json json-str)]
+            [http.async.client :as c]))
+
+(defn- validate-response [response]
+  (let [error (c/error response)]
+    (if error
+      (throw (Exception. (.getMessage error)))
+      response)))
+
+(defn- validate-response-code [response]
+  (let [status (c/status response)]
+    (if (= 200 (:code status))
+      response
+      (throw (Exception. (str "HTTP Response status is " (:code status) " from " (:url response)))))))
+
+(defn get-string-response
+  ([url] (get-json-response-as-map url nil))
+  ([url cookies]
+    (with-open [client (c/create-client)]
+      (let [response-data (-> (c/GET client url :cookies cookies)
+                            c/await
+                            validate-response
+                            validate-response-code
+                            c/string)]
+        (if (empty? response-data)
+          (throw (Exception. "No Data Returned"))
+          response-data)
+        ))))
