de94d564e21406fc598cdeea38f36baba661a633
Hugo <hugo.firth@gmail.com>
2013-08-24 20:30:33 +1000

    added jetty to deps

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [org.clojure/data.json "0.2.2"]
-                 [http.async.client "0.5.2"]]
+                 [http.async.client "0.5.2"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
   :plugins [[lein-ring "0.8.5"]]
   :min-lein-version "2.0.0"
   :uberjar-name "map-td-transform.jar"
