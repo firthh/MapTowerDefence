1cf9b4bd1c1405247d89d7c3fc0972501259af72
Hugo Firth <hugo.firth@gmail.com>
2014-02-17 13:31:17 +1000

    removed uneeded div in html. updated formatting in core

--- src/clj_transform/core.clj
+++ src/clj_transform/core.clj
@@ -1,8 +1,8 @@
 (ns clj-transform.core
   (:require [clojure.xml :as xml]
             [clojure.zip :as zip]
-            [clj-transform.http-wrapper :as http])
-  )
+            [clj-transform.http-wrapper :as http]
+            ))
 
 ;;copy-pasta from clojure docs
 (defn zip-str [s]
@@ -12,13 +12,13 @@
   (let [parsed-data (zip-str xml-data)
         non-nil-vals (filter #(not= % nil) parsed-data)]
     (first non-nil-vals)
- ))
+    ))
 
 (defn get-osm-data [left bottom right top]
   (->> (str "http://api.openstreetmap.org/api/0.6/map?bbox=" left "," bottom "," right "," top)
       http/get-string-response
       x-to-j
       :content
-      (group-by :tag)) 
-)
+      (group-by :tag)
+      ))
 
